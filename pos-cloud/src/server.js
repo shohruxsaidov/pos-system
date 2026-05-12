d9f2aaee1704d7ca219c87f8b766596918e82d26
shohrux <seltashoh@gmail.com>
2026-05-13 00:40:25 +0500

    feat: add shutdown hooks and database pool cleanup in server.js

--- pos-cloud/src/server.js
+++ pos-cloud/src/server.js
@@ -2,6 +2,7 @@ import 'dotenv/config'
 import Fastify from 'fastify'
 import fjwt from '@fastify/jwt'
 import { runMigrations } from './db/migrate.js'
+import { pool } from './db/connection.js'
 import syncRoutes from './routes/sync.js'
 import healthRoutes from './routes/health.js'
 import reportsRoutes from './routes/reports.js'
@@ -13,6 +14,25 @@ fastify.register(fjwt, { secret: process.env.JWT_SECRET || 'change-this-jwt-secr
 fastify.register(healthRoutes)
 fastify.register(syncRoutes)
 fastify.register(reportsRoutes)
+
+fastify.addHook('onClose', async () => {
+  await pool.end()
+  fastify.log.info('Database pool closed')
+})
+
+const shutdown = async (signal) => {
+  fastify.log.info({ signal }, 'Shutting down')
+  try {
+    await fastify.close()
+    process.exit(0)
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+
+process.on('SIGTERM', () => shutdown('SIGTERM'))
+process.on('SIGINT', () => shutdown('SIGINT'))
 
 const start = async () => {
   try {
